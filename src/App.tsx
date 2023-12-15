0ff0d5939a10a194df304da8b3e52bfb33f27d97
cyruscook <11858105+cyruscook@users.noreply.github.com>
2023-12-15 15:14:42 +0000

    If inputs aren't in URL parameters provide a form

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,33 @@ import { ErrorBoundary } from "react-error-boundary";
 import { S3Client } from "@aws-sdk/client-s3";
 import ClientContext from "./ClientContext.tsx";
 import BucketList from "./BucketList.tsx";
+import InputForm from "./InputForm.tsx";
 import "./App.css";
 
 export default function App() {
+	function fallbackRender(props: { error: { message: string } }) {
+		console.error(props.error);
+		return (
+			<div role="alert">
+				<p>Something went wrong:</p>
+				<pre style={{ color: "red" }}>{props.error.message}</pre>
+			</div>
+		);
+	}
+
 	const queryParameters = new URLSearchParams(window.location.search);
 	const region = queryParameters.get("region");
 	const accessKeyId = queryParameters.get("accessKeyId");
 	const secretAccessKey = queryParameters.get("secretAccessKey");
 	const bucket = queryParameters.get("bucket");
-	if (region === null) {
+
+	if (region === null || accessKeyId === null || secretAccessKey === null || bucket === null) {
 		return (
-			<p>Please provide the "region" query parameter with the S3 bucket's region.</p>
-		);
-	}
-	if (accessKeyId === null || secretAccessKey === null || bucket === null) {
-		return (
-			<p>Please provide the "accessKeyId" and "secretAccessKey" query parameters with the AWS credentials.</p>
-		);
-	}
-	if (bucket === null) {
-		return (
-			<p>Please provide the "bucket" query parameter with the S3 bucket name.</p>
+			<ErrorBoundary fallbackRender={fallbackRender}>
+				<div className="App">
+					<InputForm region={region} accessKeyId={accessKeyId} secretAccessKey={secretAccessKey} bucket={bucket} />
+				</div>
+			</ErrorBoundary>
 		);
 	}
 
@@ -33,16 +39,6 @@ export default function App() {
 			secretAccessKey: secretAccessKey,
 		},
 	});
-
-	function fallbackRender(props: { error: { message: string } }) {
-		console.error(props.error);
-		return (
-			<div role="alert">
-				<p>Something went wrong:</p>
-				<pre style={{ color: "red" }}>{props.error.message}</pre>
-			</div>
-		);
-	}
 
 	return (
 		<ClientContext.Provider value={client}>
---
+++ src/InputForm.tsx
@@ -0,0 +1,62 @@
+import { useState, FormEvent } from "react";
+
+
+export default function InputForm(props: { region: string | null, accessKeyId: string | null, secretAccessKey: string | null, bucket: string | null }) {
+    const [error, setError] = useState<string | null>(null);
+
+    function submit(event: FormEvent) {
+        event.preventDefault();
+        const formData = new FormData(event.target as HTMLFormElement);
+        const { region, accessKeyId, secretAccessKey, bucket } = Object.fromEntries(formData);
+        if (!region) {
+            setError("Invalid Bucket Region");
+        } else if (!accessKeyId) {
+            setError("Invalid Access Key ID");
+        } else if (!secretAccessKey) {
+            setError("Invalid Secret Access Key");
+        } else if (!bucket) {
+            setError("Invalid Bucket");
+        } else {
+            const url = new URL(window.location.href);
+            url.searchParams.set("region", region as string);
+            url.searchParams.set("accessKeyId", accessKeyId as string);
+            url.searchParams.set("secretAccessKey", secretAccessKey as string);
+            url.searchParams.set("bucket", bucket as string);
+            window.history.pushState({ path: url.href }, '', url.href);
+            window.history.go();
+        }
+    }
+
+    return (
+        <>
+            {error && <p>{error}</p>}
+            <form className="inputform" onSubmit={submit}>
+                <label>
+                    Bucket Region:
+                    <br />
+                    <input type="text" name="region" value={props.region ?? undefined} required />
+                </label>
+
+                <label>
+                    Access Key ID:
+                    <br />
+                    <input type="text" name="accessKeyId" value={props.accessKeyId ?? undefined} required />
+                </label>
+
+                <label>
+                    Secret Access Key:
+                    <br />
+                    <input type="text" name="secretAccessKey" value={props.secretAccessKey ?? undefined} required />
+                </label>
+
+                <label>
+                    Bucket:
+                    <br />
+                    <input type="text" name="bucket" value={props.bucket ?? undefined} required />
+                </label>
+
+                <button type="submit">Submit</button>
+            </form>
+        </>
+    );
+}
